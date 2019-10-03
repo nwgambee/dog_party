e83aba9540f160b257285f6c08391700a6173ddf
Noah Gambee <noah@gambee.dev>
2019-10-03 14:24:42 -0600

    Fix submit dog name function

--- main.js
+++ main.js
@@ -8,4 +8,5 @@ document.getElementById("submit").addEventListener("click", changeDogName);
 function changeDogName() {
 var newDogName = document.getElementById("name").value;
 document.getElementById("change_this_name").innerHTML = newDogName;
+event.preventDefault();
 };
